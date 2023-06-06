0a290bceafba13b5930e85db9f99ac7ea570a676
Stefan Sentic <stefan.sentic@htecgroup.com>
2023-06-06 12:36:08 +0200

    implement mi-card using Card+ListTile widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,81 +12,57 @@ class MiCardApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            children: [
-              const CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('lib/mi_card/image/john_doe.jpg'),
-              ),
-              const Text(
-                'John Doe',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+              children: [
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('lib/mi_card/image/john_doe.jpg'),
                 ),
-              ),
-              Text(
-                'Account manager',
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  fontSize: 20,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal.shade100,
-                ),
-              ),
-              const Card(
-                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        '+381 61 234 23 23',
-                        style: TextStyle(
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20,
-                          color: Colors.teal,
-                        ),
-                      )
-                    ],
+                const Text(
+                  'John Doe',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              const Card(
-                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.email,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        'john.doe@gmail.com',
-                        style: TextStyle(
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20,
-                          color: Colors.teal,
-                        ),
-                      )
-                    ],
+                Text(
+                  'Account manager',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal.shade100,
                   ),
                 ),
-              ),
-            ],
+                const Card(
+                  margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                  child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal),
+                    title: Text(
+                      '+381 61 234 23 23',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Source Sans Pro',
+                          color: Colors.teal),
+                    ),
+                  ),
+                ),
+                const Card(
+                  margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text(
+                      'john.doe@gmail.com',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Source Sans Pro',
+                          color: Colors.teal),
+                    ),
+                  ),
+                ),
+              ]
           ),
         ),
       ),
