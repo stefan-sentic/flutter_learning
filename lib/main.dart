541e35259ba038b8a514e79cb1af36f042d0dc49
Stefan Sentic <stefan.sentic@htecgroup.com>
2023-06-06 11:20:12 +0200

    create mi-card app with containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,95 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: const Text("I Am Rich"),
-        backgroundColor: Colors.blueGrey[900],
-      ),
-      body: const Center(
-        child: Image(
-          image: AssetImage('lib/i_am_rich/diamond.png'),
+void main() => runApp(const MiCardApp());
+
+class MiCardApp extends StatelessWidget {
+  const MiCardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('lib/mi_card/image/john_doe.jpg'),
+              ),
+              const Text(
+                'John Doe',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Account manager',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(10),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+381 61 234 23 23',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                        color: Colors.teal,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(10),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'john.doe@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                        color: Colors.teal,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  ),
-);
+    );
+  }
+}
