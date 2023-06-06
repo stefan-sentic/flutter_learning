cf4979236335aa1e4eba50bc4421fa7da19a6723
Stefan Sentic <stefan.sentic@htecgroup.com>
2023-06-06 12:46:53 +0200

    finish mi-card app

--- lib/main.dart
+++ lib/main.dart
@@ -11,59 +11,64 @@ class MiCardApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-              children: [
-                const CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('lib/mi_card/image/john_doe.jpg'),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('lib/mi_card/image/john_doe.jpg'),
+            ),
+            const Text(
+              'John Doe',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Account manager'.toUpperCase(),
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                fontSize: 20,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+                color: Colors.teal.shade100,
+              ),
+            ),
+                Divider(
+                  thickness: 1,
+                  indent: 60,
+                  endIndent: 60,
+                  height: 20,
+                  color: Colors.teal.shade100,
                 ),
-                const Text(
-                  'John Doe',
+            const Card(
+              margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.phone, color: Colors.teal),
+                title: Text(
+                  '+381 61 234 23 23',
                   style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
+                      fontSize: 20,
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.teal),
                 ),
-                Text(
-                  'Account manager',
+              ),
+            ),
+            const Card(
+              margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.email, color: Colors.teal),
+                title: Text(
+                  'john.doe@gmail.com',
                   style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    fontSize: 20,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.teal.shade100,
-                  ),
+                      fontSize: 20,
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.teal),
                 ),
-                const Card(
-                  margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                  child: ListTile(
-                    leading: Icon(Icons.phone, color: Colors.teal),
-                    title: Text(
-                      '+381 61 234 23 23',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: 'Source Sans Pro',
-                          color: Colors.teal),
-                    ),
-                  ),
-                ),
-                const Card(
-                  margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                  child: ListTile(
-                    leading: Icon(Icons.email, color: Colors.teal),
-                    title: Text(
-                      'john.doe@gmail.com',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: 'Source Sans Pro',
-                          color: Colors.teal),
-                    ),
-                  ),
-                ),
-              ]
-          ),
+              ),
+            ),
+          ]),
         ),
       ),
     );
